c049b53b5e08fd6d5dcb86f64f9a2fdbfff9b4d4
MarkZhang <mark.bowen.zhang@gmail.com>
2020-05-19 18:28:26 +0100

    Time complexity O(nlogn) -- sorting   O(n^2)--two pointers

--- 16.3sum-closest/src/Solution.java
+++ 16.3sum-closest/src/Solution.java
@@ -1,20 +1,6 @@
-// 16. 3Sum Closest
-// Medium
+//Sorting with two pointers 
+//Time complexity O(nlogn) -- sorting   O(n^2)--two pointers
 
-// 1892
-
-// 133
-
-// Add to List
-
-// Share
-// Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
-
-// Example:
-
-// Given array nums = [-1, 2, 1, -4], and target = 1.
-
-// The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 
 import java.util.*;
 
