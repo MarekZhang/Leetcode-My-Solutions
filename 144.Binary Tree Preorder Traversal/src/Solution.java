0d5bc25b2b5334c343d412464e59af2481bc7f74
MarkZhang <mark.bowen.zhang@gmail.com>
2020-06-25 13:15:22 +0100

    simulate function invocation in an operating system

---
+++ 144.Binary Tree Preorder Traversal/src/Solution.java
@@ -0,0 +1,77 @@
+/**
+ * 144. Binary Tree Preorder Traversal
+Medium
+
+1458
+
+56
+
+Add to List
+
+Share
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+ */
+
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+import java.util.*;
+
+class Solution {
+    class Command{
+        String s;
+        TreeNode node;
+        Command(String s, TreeNode node){
+            this.s = s;
+            this.node = node;
+        }
+    }   
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null)
+            return list;
+        Stack<Command> stack = new Stack<>();
+
+        stack.push(new Command("go", root));
+
+        while(!stack.isEmpty()) {
+            Command command = stack.pop();
+            if(command.s.equals("print"))
+                list.add(command.node.val);
+            else{//程序入栈顺序和执行顺序是反的，入栈的是命令，入栈的是命令，入栈的是命令
+                if(command.node.right != null)
+                    stack.push(new Command("go", command.node.right));
+                if(command.node.left!=null)
+                    stack.push(new Command("go", command.node.left));
+                stack.push(new Command("print", command.node));
+            }
+        }
+        return list;
+    }
+
+}
