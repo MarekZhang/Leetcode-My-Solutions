b4ff5e1cbf365659ff75d0f21c1660cb29ffb519
MarkZhang <mark.bowen.zhang@gmail.com>
2020-07-25 23:23:29 +0100

    trackback

---
+++ 216.Combination Sum III/src/Solution.java
@@ -0,0 +1,56 @@
+/**
+ * 216. Combination Sum III
+Medium
+
+1061
+
+51
+
+Add to List
+
+Share
+Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+
+Note:
+
+All numbers will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: k = 3, n = 7
+Output: [[1,2,4]]
+Example 2:
+
+Input: k = 3, n = 9
+Output: [[1,2,6], [1,3,5], [2,3,4]]
+ */
+
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        if(n < k)
+            return res;
+        
+        LinkedList<Integer> tempt = new LinkedList<>();
+        dfs(k, n, tempt);
+
+        return res;
+    }
+
+    private void dfs(int k, int n, LinkedList<Integer> tempt){
+        if(tempt.size() == k){
+            if(n == 0)
+                res.add(tempt);
+        }
+
+        for(int i = 1; i <= n - (k - tempt.size()); i++){
+            tempt.addLast(i);
+            dfs(k - i, n, tempt);
+            tempt.removeLast();
+        }
+
+
+        return;
+    }
+}
