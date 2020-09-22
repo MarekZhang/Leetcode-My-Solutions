1e660e0600e92a3a85d3dd8fed347696027ba828
MarkZhang <mark.bowen.zhang@gmail.com>
2020-09-22 22:58:22 +0100

    recursion with memory search

---
+++ 1143. Longest Common Subsequence/src/Solution.java
@@ -0,0 +1,71 @@
+/**
+ * 1143. Longest Common Subsequence
+Medium
+
+1891
+
+25
+
+Add to List
+
+Share
+Given two strings text1 and text2, return the length of their longest common subsequence.
+
+A subsequence of a string is a new string generated from the original string with some characters(can be none) deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not). A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+If there is no common subsequence, return 0.
+
+ 
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+ */
+
+class Solution {
+    //charAt(m)== charAt(n) -- LCS(m,n) = 1 + LCS(m-1, n-1) 
+    //charAt(m)!= charAt(n) --  max(LCS(m, n-1), LCS(m-1, n))
+    private int[][] memo;
+    public int longestCommonSubsequence(String text1, String text2) {
+      if(text1 == null || text2 == null || text1.length()== 0 || text2.length()== 0)  
+        return 0;
+      
+      //memo[m][n] represents the LCS value between text1.substring(0, m) and text2.substring(0,n)
+      memo = new int[text1.length()][text2.length()];
+      for(int i = 0; i < text1.length(); i++)
+        Arrays.fill(memo[i], -1);
+
+      return LCS(text1, text2);
+    }
+
+    private int LCS(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        if(m == 0 || n == 0)
+            return 0;
+
+        if(memo[m - 1][n - 1] != -1)
+            return memo[m - 1][n - 1];
+
+        if(text1.charAt(m - 1) == text2.charAt(n - 1))
+            memo[m - 1][n - 1] = 1 + LCS(text1.substring(0, m - 1), text2.substring(0, n - 1));
+        else
+            memo[m -1][n - 1] = Math.max(LCS(text1.substring(0, m-1), text2), LCS(text1, text2.substring(0, n-1)));
+        
+        return memo[m - 1][n - 1];
+    }
+}
