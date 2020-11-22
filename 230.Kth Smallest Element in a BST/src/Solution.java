aaf01ebd0678550655f720c5baa049486fe3ad37
MarkZhang <mark.bowen.zhang@gmail.com>
2020-11-22 12:58:12 +0000

    [solution update]

--- 230.Kth Smallest Element in a BST/src/Solution.java
+++ 230.Kth Smallest Element in a BST/src/Solution.java
@@ -45,33 +45,21 @@ You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
  */
 
 class Solution {
+    private int count;
+    //inorder | time complexity O(N) | space complexity O(H) -- stack space the maximum deepth of the tree
     public int kthSmallest(TreeNode root, int k) {
-        int nodes = numOfNodes(root.left);
-        if(nodes == k)
-            return findMax(root.left).val;
-        else if(nodes > k)
-            return kthSmallest(root.left, k);
-        else if(nodes + 1 == k)
-            return root.val;
-        // k > nodes + 1
-        return kthSmallest(root.right, k - nodes - 1);
+        return inorder(root, k).val;
     }
-
-    private int numOfNodes(TreeNode root) {
-        if(root == null)
-            return 0;
-        if(root.left == null && root.right == null)
-            return 1;
-        
-        return numOfNodes(root.left) + numOfNodes(root.right) + 1;
-    }
-
-    private TreeNode findMax(TreeNode root){
-        if(root == null)
+    
+    private TreeNode inorder(TreeNode node, int k){
+        if(node == null)
             return null;
-        if(root.left == null && root.right == null)
-            return root;
-
-        return findMax(root.right);
+        TreeNode left = inorder(node.left, k);
+        count++;
+        if(count == k)
+            return node;
+        TreeNode right = inorder(node.right, k);
+        
+        return left == null ? right : left;
     }
 }
